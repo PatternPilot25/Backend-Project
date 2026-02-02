b563f24fde908aefe3a1ce89f3922d5067018f3c
Chaman Jha <jhachaman163@gmail.com>
2026-02-02 12:31:31 +0530

    Remove console.log statements for user registration process to clean up code

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -9,8 +9,8 @@ const userRegister=asyncHandler(async(req,res)=>{
     
   // get the user data from frontend using req.body if data is sent through body (json or form data)
        const {userName, email, fullName, password } = req.body
-     console.log(req.body)
-     console.log(req.files)
+   //  console.log(req.body)
+    // console.log(req.files)
     //  check the validation of data especially its empty or not
       if(
         [userName,email,fullName,password].some((field)=> field?.trim()==="") // field => !field || field.trim() === ""
@@ -30,21 +30,21 @@ const userRegister=asyncHandler(async(req,res)=>{
     // check whether the files are present or not in local storage specially avatar as it is mandatory
     // req.files is an object that contains arrays of files (avatar & cover image) these arrays contains objects of file details at index 0 & then this [0] index contains path property which contains the local path of uploaded file
     const avatarLocalpath=req.files?.avatar[0]?.path
-    console.log("avatar local path:",avatarLocalpath)
+      // console.log("avatar local path:",avatarLocalpath)
     let coverLocalpath;
      if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
       coverLocalpath= req.files.coverImage[0].path
      }
-    console.log("cover image local path:",coverLocalpath)
+   // console.log("cover image local path:",coverLocalpath)
     if(!avatarLocalpath){
        throw new ApiError(400,"avatar file is required")
     }
     // upload the avatar and cover image to cloudinary and get the url of uploaded image from cloudinary response
     // check whether the upload is successful or not in cloudinary
    const avatarlink= await uploadOncloudinary(avatarLocalpath) // only response comes
-   console.log("avatar link from cloudinary:",avatarlink) 
+   //  console.log("avatar link from cloudinary:",avatarlink) 
    const coverlink =await uploadOncloudinary(coverLocalpath)  
-   console.log("cover image link from cloudinary:",coverlink)  // only reponse comes
+  // console.log("cover image link from cloudinary:",coverlink)  // only reponse comes
    if(!avatarlink) throw new ApiError (500,"Error in uploading avatar image")
 
     
